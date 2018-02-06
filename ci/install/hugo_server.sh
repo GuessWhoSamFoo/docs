2cb5bd79d7ee92cc8edd57419e34d830e6a1e248
Sam Foo <sfoo@linode.com>
2018-02-06 14:51:25 -0500

    Trigger rebuild

--- ci/install/hugo_server.sh
+++ ci/install/hugo_server.sh
@@ -5,6 +5,7 @@ wget https://github.com/gohugoio/hugo/releases/download/v0.36/hugo_0.36_Linux-64
 sudo dpkg -i hugo*.deb
 hugo version
 
+# Allow Hugo to start when running CI
 echo "Setting up local development server"
 hugo server &
 sleep 7
